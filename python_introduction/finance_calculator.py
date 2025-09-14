108b8d649f9eae355ecf7a712a1bfbb8f0ecafd9
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-14 22:27:39 +0300

    Fixing Error in Task 5

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-user_income = float(input("Enter your monthly income: "))
-user_expense = float(input("Enter your monthly Expenses: "))
-monthly_savings = user_income - user_expense
-year_savings = monthly_savings *12 + (monthly_savings *12 * 0.05)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly Expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+year_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print(f"Your monthly savings are ${monthly_savings}")
 print(f"Projected savings after one year, with interest, is: ${year_savings}")
