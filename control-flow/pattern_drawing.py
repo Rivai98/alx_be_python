451165afd64d4cca27675141f69e1f06e12d40d3
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-22 00:35:43 +0300

    Task 3 Bug fixing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,18 @@
-number_patterns = int(input("Enter the size of the pattern:"))
+# Pattern Drawing Script
+# This script uses nested loops to draw a square pattern of asterisks
 
-for i in range(number_patterns):
-    for j in range(number_patterns):
-        print("*",end="")
+# Prompt user for pattern size
+size = int(input("Enter the size of the pattern: "))
+
+# Draw the pattern using nested loops
+row = 0
+while row < size:
+    # Use for loop to print asterisks in each row
+    for col in range(size):
+        print("*", end="")
+    
+    # Move to the next line after completing each row
     print()
+    
+    # Increment row counter
+    row += 1
