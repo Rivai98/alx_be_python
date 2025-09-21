3183a6595542891da12375533b487fb325398ec9
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-22 00:44:40 +0300

    Last Commit Fix.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,28 +7,28 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         reminder = f"'{task}' is a high priority task"
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             reminder += " that requires immediate attention today!"
         else:
             reminder += ". Complete it as soon as possible."
     
     case "medium":
         reminder = f"'{task}' is a medium priority task"
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             reminder += " that requires immediate attention today!"
         else:
             reminder += ". Consider completing it when you have free time."
     
     case "low":
         reminder = f"'{task}' is a low priority task"
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             reminder += " that requires immediate attention today!"
         else:
             reminder += ". Complete it when you have time."
     
     case _:
         reminder = f"'{task}' has an unrecognized priority level"
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             reminder += " that requires immediate attention today!"
 
 # Provide the customized reminder
