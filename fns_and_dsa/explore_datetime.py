f9db42ac787516e089cada358459279ab377eb39
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-28 23:02:29 +0300

    Edifting Task 2

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,21 @@
 from datetime import datetime,timedelta
-current_date = datetime.now()
-print("Current date and time:", current_date)
 
-number_of_days= int(input("Enter the number of days to add to the current date:"))
-duration=timedelta(days=number_of_days,) 
-future_time= current_date+duration
-print("Future date:", future_time) 
+current_date = datetime.now()
+def display_current_datetime():
+    print("Current date and time:", current_date)
+
+
+
+
+def calculate_future_date():
+    number_of_days= int(input("Enter the number of days to add to the current date:"))
+    duration=timedelta(days=number_of_days) 
+    future_time= current_date+duration
+    print("Future date:", future_time) 
+
+
+
+
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
